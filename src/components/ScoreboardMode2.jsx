c6fd4e413070d6bb771e1ebd803eff2b8080c5a2
entercon-technical <techentercon@gmail.com>
2026-09-20 17:11:17 +0530

    Update ScoreboardMode2.jsx

--- src/components/ScoreboardMode2.jsx
+++ src/components/ScoreboardMode2.jsx
@@ -27,7 +27,7 @@ export default function ScoreboardMode2() {
   const role = location.state?.role || "Admin"; // Default to Admin for backward compatibility
 
   axios
-    .get("https://entercon-backend.onrender.com/get-data")
+    .get("https://entercon-backend-1e81.onrender.com/get-data")
     .then((response) => setSchools(response.data))
     .catch((e) => console.log(e));
 
